ae9ed4cf43504ebf1903b39212b47167743bdf12
titi <titi@pince.(none)>
2012-11-02 07:49:28 +0100

    better display

--- pyscreenshot/check/speedtest.py
+++ pyscreenshot/check/speedtest.py
@@ -7,12 +7,12 @@ import time
 
 
 def run(force_backend, n, to_file, bbox=None):
-    print force_backend,' '*(20-len(force_backend)),
+    print force_backend, ' ' * (20 - len(force_backend)),
 
     BackendLoader().force(force_backend)
-        
+
     f = tempfile.NamedTemporaryFile(suffix='.png', prefix='test')
-    filename=f.name
+    filename = f.name
     start = time.time()
     for i in range(n):
         if to_file:
@@ -20,11 +20,13 @@ def run(force_backend, n, to_file, bbox=None):
         else:
             pyscreenshot.grab(bbox=bbox)
     end = time.time()
-    print int(10*(end - start))/10.0, 'sec'
+    dt = end - start
+    print '%.2g sec' % (dt), '(%d ms per call)' % (1000.0 * dt / n)
+
 
 def run_all(n, to_file, bbox=None):
     print
-    print 'n=%s' %  n, ', to_file:', to_file,', bounding box:', bbox
+    print 'n=%s' % n, ', to_file:', to_file, ', bounding box:', bbox
     print '------------------------------------------------------'
 
     backends = BackendLoader().all_names
@@ -34,14 +36,11 @@ def run_all(n, to_file, bbox=None):
 #            print 'grabbing by '+x
         except PluginLoaderError as e:
             print e
-    
+
 
 @entrypoint
-def speedtest(): 
-    n=10   
+def speedtest():
+    n = 10
     run_all(n, True)
     run_all(n, False)
-    run_all(n, False, (10,10,20,20))
-
-
-
+    run_all(n, False, (10, 10, 20, 20))
