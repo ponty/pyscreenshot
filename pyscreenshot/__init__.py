5214e3e0c9213514adf6131639af256cd4067536
ponty <ponty@home>
2013-02-16 11:33:15 +0100

    refactor

--- pyscreenshot/__init__.py
+++ pyscreenshot/__init__.py
@@ -11,6 +11,41 @@ log = logging.getLogger(__name__)
 log.debug('version=' + __version__)
 
 
+def _grab(to_file, childprocess=False, backend=None, bbox=None, filename=None):
+    if childprocess:
+        log.debug('running "%s" in child process' % backend)
+
+        if not to_file:
+            f = tempfile.NamedTemporaryFile(
+                suffix='.png', prefix='pyscreenshot_childprocess_')
+            filename = f.name
+
+        params = ["'%s'" % (filename), 'childprocess=False']
+        if backend:
+            params += ["backend='%s'" % (backend)]
+        params = ','.join(params)
+
+        EasyProcess([sys.executable,
+                     '-c',
+                     "import pyscreenshot; pyscreenshot.grab_to_file(%s)" % (
+                     params),
+                     ]).check()
+        if not to_file:
+            im = Image.open(filename)
+            if bbox:
+                im = im.crop(bbox)
+            return im
+    else:
+        if backend:
+            BackendLoader().force(backend)
+        backend_obj = BackendLoader().selected()
+
+        if to_file:
+            return backend_obj.grab_to_file(filename)
+        else:
+            return backend_obj.grab(bbox)
+
+
 def grab(bbox=None, childprocess=False, backend=None):
     '''Copy the contents of the screen to PIL image memory.
 
@@ -22,28 +57,7 @@ def grab(bbox=None, childprocess=False, backend=None):
     :param backend: back-end can be forced if set (examples:scrot, wx,..),
                     otherwise back-end is automatic
     '''
-    if childprocess:
-        f = tempfile.NamedTemporaryFile(
-            suffix='.png', prefix='pyscreenshot_childprocess_')
-        filename = f.name
-
-        params = ["'%s'" % (filename), 'childprocess=False']
-        if backend:
-            params += ["backend='%s'" % (backend)]
-        params = ','.join(params)
-
-        EasyProcess([sys.executable, 
-                     '-c',
-                     "import pyscreenshot; pyscreenshot.grab_to_file(%s)" % params,
-                     ]).check()
-        im = Image.open(filename)
-        if bbox:
-            im = im.crop(bbox)
-        return im
-    else:
-        if backend:
-            BackendLoader().force(backend)
-        return BackendLoader().selected().grab(bbox)
+    return _grab(to_file=False, childprocess=childprocess, backend=backend, bbox=bbox)
 
 
 def grab_to_file(filename, childprocess=False, backend=None):
@@ -53,18 +67,4 @@ def grab_to_file(filename, childprocess=False, backend=None):
     :param childprocess: see :py:func:`grab`
     :param backend: see :py:func:`grab`
     '''
-    if childprocess:
-
-        params = ["'%s'" % (filename), 'childprocess=False']
-        if backend:
-            params += ["backend='%s'" % (backend)]
-        params = ','.join(params)
-
-        EasyProcess([sys.executable, 
-                     '-c',
-                     "import pyscreenshot; pyscreenshot.grab_to_file(%s)" % params,
-                     ]).check()
-    else:
-        if backend:
-            BackendLoader().force(backend)
-        return BackendLoader().selected().grab_to_file(filename)
+    return _grab(to_file=True, childprocess=childprocess, backend=backend, filename=filename)
