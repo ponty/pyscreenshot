a4a77087788752a98fbb8e9ba971813e7cc36c04
ponty <ponty@home>
2015-03-26 13:09:19 +0100

    more test

---
+++ tests/anybackend/test_examples.py
@@ -0,0 +1,13 @@
+from pyscreenshot.examples import show, showall
+from pyvirtualdisplay.display import Display
+
+
+def test_show():
+    with Display(visible=0, size=(800, 600)):
+        show.show()
+
+def test_showall():
+    with Display(visible=0, size=(800, 600)):
+        showall.show()
+
+
