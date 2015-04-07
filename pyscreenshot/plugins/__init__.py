f70cc604c69b60513542040497b9650838fb1c4e
ponty <ponty@home>
2015-04-07 16:03:11 +0200

    platform specific backends

--- pyscreenshot/plugins/__init__.py
+++ pyscreenshot/plugins/__init__.py
@@ -6,17 +6,47 @@ import scrot
 import wxscreen
 import mac_screencapture
 import mac_quartz
+import sys
 
 
-BACKENDS = [
-    pil.PilWrapper,
-    scrot.ScrotWrapper,
-    wxscreen.WxScreen,
-    gtkpixbuf.GtkPixbufWrapper,
-    qtgrabwindow.QtGrabWindow,
-    imagemagick.ImagemagickWrapper,
-    mac_screencapture.ScreencaptureWrapper,
-    mac_quartz.MacQuartzWrapper,
-]
+if sys.platform == 'linux2':
+    BACKENDS = [
+        wxscreen.WxScreen,
+        gtkpixbuf.GtkPixbufWrapper,
+        qtgrabwindow.QtGrabWindow,
+        scrot.ScrotWrapper,
+        imagemagick.ImagemagickWrapper,
+    ]
+elif sys.platform == 'darwin':
+    BACKENDS = [
+        mac_quartz.MacQuartzWrapper,
+        mac_screencapture.ScreencaptureWrapper,
+        wxscreen.WxScreen,
+        gtkpixbuf.GtkPixbufWrapper,
+        qtgrabwindow.QtGrabWindow,
+        scrot.ScrotWrapper,
+        imagemagick.ImagemagickWrapper,
+    ]
+elif sys.platform == 'win32':
+    BACKENDS = [
+        pil.PilWrapper,
+        wxscreen.WxScreen,
+        gtkpixbuf.GtkPixbufWrapper,
+        qtgrabwindow.QtGrabWindow,
+        scrot.ScrotWrapper,
+        imagemagick.ImagemagickWrapper,
+    ]
+else:
+    BACKENDS = [
+        pil.PilWrapper,
+        wxscreen.WxScreen,
+        gtkpixbuf.GtkPixbufWrapper,
+        qtgrabwindow.QtGrabWindow,
+        scrot.ScrotWrapper,
+        imagemagick.ImagemagickWrapper,
+        mac_screencapture.ScreencaptureWrapper,
+        mac_quartz.MacQuartzWrapper,
+    ]
+
 
 default_preference = [x.name for x in BACKENDS]
