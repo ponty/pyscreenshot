a46191b8a27960bd2f5f75ca8cc5e6312220bff4
yueranyuan <yueranyuan@gmail.com>
2018-09-22 14:34:28 +0530

    updated wx plugin to be up-to-date with the latest wx and Pillow

--- pyscreenshot/plugins/wxscreen.py
+++ pyscreenshot/plugins/wxscreen.py
@@ -21,15 +21,21 @@ class WxScreen(object):
             self.app = wx.App()
         screen = wx.ScreenDC()
         size = screen.GetSize()
-        bmp = wx.EmptyBitmap(size[0], size[1])
+        if hasattr(wx, "Bitmap"):
+            bmp = wx.Bitmap(size[0], size[1])
+        else:
+            bmp = wx.EmptyBitmap(size[0], size[1])
         mem = wx.MemoryDC(bmp)
         mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
         del mem
-        myWxImage = wx.ImageFromBitmap(bmp)
+        if hasattr(bmp, "ConvertToImage"):
+            myWxImage = bmp.ConvertToImage()
+        else:
+            myWxImage = wx.ImageFromBitmap(bmp)
         im = Image.new('RGB', (myWxImage.GetWidth(), myWxImage.GetHeight()))
         if hasattr(Image, 'frombytes'):
             # for Pillow
-            im.frombytes(myWxImage.GetData())
+            im.frombytes(buffer(myWxImage.GetData()))
         else:
             # for PIL
             im.fromstring(myWxImage.GetData())
