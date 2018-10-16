1e7f9d13d0e118d0d75225f69628680c8af3a8ae
ponty <ponty@home>
2018-10-16 15:39:38 +0200

    test: fix name

--- tests/test_pyside.py
+++ tests/test_pyside.py
@@ -2,9 +2,9 @@ from ref import backend_ref
 from size import backend_size
 
 
-def test_size_pyqt():
+def test_size_pyside():
     backend_size('pyside')
 
 
-def test_ref_pyqt():
+def test_ref_pyside():
     backend_ref('pyside')
