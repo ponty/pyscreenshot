fcf167fc1d49b85b84590134cdd28c4ad26843a0
ponty <ponty@home>
2019-12-04 10:07:38 +0100

    test: no pygtk for py3

--- tests/test_pygtk.py
+++ tests/test_pygtk.py
@@ -1,10 +1,13 @@
 from ref import backend_ref
 from size import backend_size
+import six
 
+# no pygtk for py3
+if six.PY2:
 
-def test_size_pygtk():
-    backend_size('pygtk')
-
-
-def test_ref_pygtk():
-    backend_ref('pygtk')
+    def test_size_pygtk():
+        backend_size('pygtk')
+    
+    
+    def test_ref_pygtk():
+        backend_ref('pygtk')
