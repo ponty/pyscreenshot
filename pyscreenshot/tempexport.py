8a02b158a97827d53a7be58c1abaa6835ac8d044
ponty <ponty@home>
2019-12-12 13:19:40 +0100

    check return code

--- pyscreenshot/tempexport.py
+++ pyscreenshot/tempexport.py
@@ -4,6 +4,10 @@ from easyprocess import EasyProcess
 from PIL import Image
 
 
+class RunProgError(Exception):
+    pass
+
+
 def read_func_img(func, bbox=None):
     with TemporaryDirectory(prefix='pyscreenshot') as tmpdirname:
         filename = os.path.join(tmpdirname, 'screenshot.png')
@@ -16,5 +20,7 @@ def read_prog_img(cmd):
     def run_prog(filename, bbox=None):
         p = EasyProcess(cmd+[filename])
         p.call()
+        if p.return_code != 0:
+            raise RunProgError(p.stderr)
     im = read_func_img(run_prog)
     return im
