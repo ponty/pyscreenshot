6113299efc432ac502188ebd00316afb54a99cc2
ponty <ponty@home>
2020-01-03 10:53:32 +0100

    doc

--- tests/test_gnome_screenshot.py
+++ tests/test_gnome_screenshot.py
@@ -6,10 +6,8 @@ from size import backend_size
 
 def test_size_gnome_screenshot():
     backend_size('gnome-screenshot', virtual_display=False)
-
-
 test_size_gnome_screenshot.real_disp_only = 1
 
-# how to call it on static screen?
+# TODO:how to call it on static screen?
 # def test_ref_gnome_screenshot():
 #     backend_ref('gnome-screenshot', virtual_display=False)
