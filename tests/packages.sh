3906338fbc91a1afcdbfae68652b6c6580ae529c
ponty <ponty@home>
2020-01-23 08:44:50 +0100

    travis: add 2.7_bionic

--- tests/packages.sh
+++ tests/packages.sh
@@ -12,8 +12,9 @@ PY=$2
 # no python3-pyside before xenial
 # no python-pyside2, python3-pyside2 before disco (19.04)
 # no python-qtpy, python3-qtpy before bionic
-U1404_PY2="python-gtk2                           python-wxgtk2.8  python-qt4                    python-pyside             "  
-U1604_PY2="python-gtk2  python-gi gir1.2-gtk-3.0 python-wxgtk3.0  python-qt4     python-pyqt5   python-pyside             "  
+U1404_PY2="python-gtk2                           python-wxgtk2.8  python-qt4                    python-pyside              "  
+U1604_PY2="python-gtk2  python-gi gir1.2-gtk-3.0 python-wxgtk3.0  python-qt4     python-pyqt5   python-pyside              "  
+U1804_PY2="python-gtk2  python-gi gir1.2-gtk-3.0 python-wxgtk3.0  python-qt4     python-pyqt5   python-pyside  python-qtpy "  
 U1604_PY3="            python3-gi gir1.2-gtk-3.0                  python3-pyqt4  python3-pyqt5  python3-pyside             " 
 U1804_PY3="            python3-gi gir1.2-gtk-3.0 python3-wxgtk4.0 python3-pyqt4  python3-pyqt5  python3-pyside python3-qtpy" 
 
@@ -33,8 +34,9 @@ if [[ ${DISTRO} == "xenial" ]];then
     fi
 fi
 if [[ ${DISTRO} == "bionic" ]];then
-    # if [[ ${PY} == "2" ]];then
-    # fi
+    if [[ ${PY} == "2" ]];then
+        echo ${U1804_PY2}
+    fi
     if [[ ${PY} == "3" ]];then
         echo ${U1804_PY3}
     fi
