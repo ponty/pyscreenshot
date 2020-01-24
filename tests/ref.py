285dcb4fefa01f9966d1ed1e9a755c527fca5c71
ponty <ponty@home>
2020-01-24 10:04:55 +0100

    test: not testing childprocess=False, all tests in 1 process

--- tests/ref.py
+++ tests/ref.py
@@ -61,4 +61,4 @@ def _backend_check(backend, childprocess):
 def backend_to_check(backend):
     fillscreen.init()
     _backend_check(backend, childprocess=True)
-    _backend_check(backend, childprocess=False)
+    # _backend_check(backend, childprocess=False) # TODO: test childprocess=False
--- tests/runxvfb.sh
+++ tests/runxvfb.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 DIST=$1
 set -e
-
-nosetests -v test_scrot.py test_imagemagick.py
-nosetests -v test_def.py
+TESTS="easy test_scrot.py test_imagemagick.py test_def.py"
 
 if [[ ${DIST} != "general" ]];then
     PYTHON_VERSION=$2
     PY=${PYTHON_VERSION:0:1}
     if [[ ${DIST} != "trusty" ]];then
-        nosetests -v test_pygdk3_conflict.py
-        nosetests -v test_pygdk3.py
-        nosetests -v test_pyqt5.py
+        TESTS="${TESTS} test_pygdk3.py test_pyqt5.py"
     fi
     if [[ ${DIST} != "xenial" ]];then
     if [[ ${PY} == "2" ]];then
-        nosetests -v test_wx.py
+        TESTS="${TESTS} test_wx.py"
     fi
     fi
-    nosetests -v test_pyqt4.py
-    nosetests -v test_pyside.py
+    TESTS="${TESTS} test_pyqt4.py test_pyside.py"
     if [[ ${PY} == "2" ]];then
-        nosetests -v test_pygtk.py
+        TESTS="${TESTS} test_pygtk.py"
     fi
 
     if [[ ${DIST} == "bionic" ]];then
-        nosetests -v test_qtpy.py
-        nosetests -v test_pyside2.py
+        TESTS="${TESTS} test_qtpy.py test_pyside2.py"
     fi
 fi
+
+
 #nosetests -v test_gnome_screenshot.py
 #nosetests -v test_mac_quartz.py
 #nosetests -v test_mac_screencapture.py
 
-nosetests -v easy
+nosetests -v  ${TESTS}
 
 
