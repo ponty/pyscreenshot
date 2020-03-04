a35e5612de2f165cb5a200d0fedcd2b3326362c0
ponty <ponty@home>
2020-03-04 12:53:31 +0100

    test: disable gnome_screenshot test

--- tests/test_gnome_screenshot.py
+++ tests/test_gnome_screenshot.py
@@ -2,13 +2,14 @@ from size import backend_size
 
 # DISPLAY variable doesn't work -> no Xvfb
 
+# TODO:enable
 
-def test_size_gnome_screenshot():
-    backend_size("gnome-screenshot", childprocess=True)
-    backend_size("gnome-screenshot", childprocess=False)
+# def test_size_gnome_screenshot():
+#     backend_size("gnome-screenshot", childprocess=True)
+#     backend_size("gnome-screenshot", childprocess=False)
 
 
-test_size_gnome_screenshot.real_disp_only = 1
+# test_size_gnome_screenshot.real_disp_only = 1
 
 # TODO:how to call it on static screen?
 # def test_ref_gnome_screenshot():
