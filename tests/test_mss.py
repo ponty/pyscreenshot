729d10fea0d0ec0aa2d415f144c2afddc1b28691
ponty <ponty@home>
2020-03-05 13:53:53 +0100

    test: move missing_RANDR()

--- tests/test_mss.py
+++ tests/test_mss.py
@@ -8,8 +8,21 @@ from pyscreenshot.util import (
     use_x_display,
 )
 
-import x
 from ref import backend_to_check, check_import
+import sys
+
+from pyscreenshot.util import platform_is_linux
+from Xlib import X, Xutil, display
+from Xlib.ext import randr
+
+
+# https://github.com/python-xlib/python-xlib/blob/master/examples/xrandr.py#L44
+def missing_RANDR():
+    if not platform_is_linux():
+        return False
+    disp = display.Display()
+    return not disp.has_extension("RANDR")
+
 
 ok = False
 if not six.PY2 and check_import("mss"):
@@ -23,7 +36,7 @@ if not six.PY2 and check_import("mss"):
 if ok:
 
     def test_mss():
-        if x.missing_RANDR():
+        if missing_RANDR():
             try:
                 backend_to_check("mss")
             except FailedBackendError:
