0a7d8940478bc5f635665ab3893e80d23255dd5f
ponty <ponty@home>
2020-03-05 10:19:55 +0100

    showall: use entrypoint, ignore all Exceptions

--- pyscreenshot/check/showall.py
+++ pyscreenshot/check/showall.py
@@ -1,25 +1,23 @@
 import time
 
 import pyscreenshot as ImageGrab
+from entrypoint2 import entrypoint
 from pyscreenshot import FailedBackendError, backends
 
 
+@entrypoint
 def show():
     im = []
 
     for x in backends():
         try:
-            print("grabbing by " + x)
+            print("--> grabbing by " + x)
             im.append(
                 ImageGrab.grab(bbox=(500, 400, 800, 600), backend=x, childprocess=True)
             )
-        except FailedBackendError as e:
+        except Exception as e:
             print(e)
     print(im)
     for x in im:
         x.show()
         time.sleep(1)
-
-
-if __name__ == "__main__":
-    show()
