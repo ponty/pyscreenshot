a17afc5fc22183c52464e52d51e3e0856cd14ce2
ponty <ponty@home>
2020-03-09 13:47:27 +0100

    test: pygdk3: more import check

--- tests/test_pygdk3.py
+++ tests/test_pygdk3.py
@@ -1,6 +1,15 @@
 from ref import backend_to_check, check_import
 
+ok = False
 if check_import("gi"):
+    import gi
+    # Arch: AttributeError: module 'gi' has no attribute 'require_version'
+    try:
+        gi.require_version
+        ok = True
+    except AttributeError:
+        pass
+if ok:
 
     def test_pygdk3():
         backend_to_check("pygdk3")
