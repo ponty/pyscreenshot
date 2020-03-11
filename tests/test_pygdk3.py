f714250daa67efce08b5a7320e28ca1368eaba06
ponty <ponty@home>
2020-03-11 12:42:43 +0100

    test: check gdk3 import

--- tests/test_pygdk3.py
+++ tests/test_pygdk3.py
@@ -1,14 +1,18 @@
+from pyscreenshot.util import use_x_display
+
 from ref import backend_to_check, check_import
 
 ok = False
 if check_import("gi"):
-    import gi
-    # Arch: AttributeError: module 'gi' has no attribute 'require_version'
-    try:
-        gi.require_version
-        ok = True
-    except AttributeError:
-        pass
+    if use_x_display():
+        import gi
+
+        # Arch: AttributeError: module 'gi' has no attribute 'require_version'
+        try:
+            gi.require_version
+            ok = True
+        except AttributeError:
+            pass
 if ok:
 
     def test_pygdk3():
