03ca86c3b2a9a6a2053ea927b703629c6e4e405e
ponty <ponty@home>
2020-03-13 12:12:45 +0100

    showall: print backend list

--- pyscreenshot/check/showall.py
+++ pyscreenshot/check/showall.py
@@ -8,14 +8,17 @@ from pyscreenshot import FailedBackendError, backends
 @entrypoint
 def show():
     im = []
+    blist = []
 
     for x in backends():
         try:
             print("--> grabbing by " + x)
             im.append(pyscreenshot.grab(bbox=(500, 400, 800, 600), backend=x))
+            blist.append(x)
         except Exception as e:
             print(e)
     print(im)
+    print(blist)
     for x in im:
         x.show()
         time.sleep(1)
