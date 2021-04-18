c948d83434d4db71ba68b75f49ed8671e07f80c1
ponty <ponty@home>
2021-04-18 11:41:46 +0200

    test:  rename gnome-screenshot test to fix later tests
    'gnome-screenshot' makes strange effects on screen.
    It can disturb later tests.
    Make this test the last by starting the name with 'x'

--- tests/test_x_gnome_screenshot.py
+++ tests/test_x_gnome_screenshot.py
@@ -1,5 +1,8 @@
 from bt import backend_to_check, gnome, kde, prog_check
 
+# 'gnome-screenshot' makes strange effects on screen.
+# It can disturb later tests.
+# Make this test the last by starting the name with 'x'
 if gnome():
 
     if prog_check(["gnome-screenshot", "--version"]):
