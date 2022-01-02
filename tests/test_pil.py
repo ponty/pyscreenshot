d3b9f28ca41febb5abb167d628975bfe3faf7f11
ponty <ponty@home>
2022-01-02 15:25:41 +0100

    test: enable test_pil() on linux

--- tests/test_pil.py
+++ tests/test_pil.py
@@ -1,7 +1,20 @@
 from bt import backend_to_check
-from pyscreenshot.util import platform_is_linux
+from pyscreenshot.util import (
+    platform_is_linux,
+    platform_is_osx,
+    platform_is_win,
+    use_x_display,
+)
 
-if not platform_is_linux():
+ok = False
+if platform_is_osx() and not use_x_display():
+    ok = True
+if platform_is_linux() and use_x_display():
+    ok = True
+if platform_is_win():
+    ok = True
+
+if ok:
 
     def test_pil():
         backend_to_check("pil")
