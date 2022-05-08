6f5adac2c36fc4def43eecc2c9a642d21331ca5b
ponty <ponty@home>
2022-05-08 22:26:31 +0200

    test_pyqt5: check_import

--- tests/test_pyqt5.py
+++ tests/test_pyqt5.py
@@ -5,7 +5,9 @@ from pyscreenshot.util import platform_is_osx
 # qt color problem on osx
 if not platform_is_osx():
 
-    if check_import("PyQt5"):
+    if check_import("PyQt5.Qt"):
+        if check_import("PyQt5.QtGui"):
+            if check_import("PyQt5.QtWidgets"):
 
-        def test_pyqt5():
-            backend_to_check("pyqt5")
+                def test_pyqt5():
+                    backend_to_check("pyqt5")
