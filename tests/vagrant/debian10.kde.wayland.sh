2ba3c6fc024ed5df45b548645e6bbda750be2f27
ponty <ponty@home>
2022-05-08 22:26:10 +0200

    ksnip test/doc

--- tests/vagrant/debian10.kde.wayland.sh
+++ tests/vagrant/debian10.kde.wayland.sh
@@ -4,8 +4,10 @@ mkdir /etc/sddm.conf.d
 echo '[Autologin]
 User=vagrant
 #Session=plasma.desktop
-Session=plasmawayland.desktop' > /etc/sddm.conf.d/autologin.conf
+Session=plasmawayland.desktop' >/etc/sddm.conf.d/autologin.conf
 
 cd /vagrant/tests/vagrant
 ./debian10_kde.sh
 sudo apt-get install -y plasma-workspace-wayland
+
+sudo apt-get install -y ksnip
