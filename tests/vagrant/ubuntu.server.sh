31e29d54e1b7956dffcecf141b2b1f8ab6af3882
ponty <ponty@home>
2023-03-01 17:10:22 +0100

    supported Python versions: 3.9, 3.10, 3.11

--- tests/vagrant/ubuntu.server.sh
+++ tests/vagrant/ubuntu.server.sh
@@ -9,14 +9,15 @@ sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en.UTF-8
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
 # sudo add-apt-repository --yes  ppa:jan-simon/pqiv
 sudo apt-get update
-sudo apt-get install -y python3.6-dev
-sudo apt-get install -y python3.7-dev
-sudo apt-get install -y python3.8-dev
-sudo apt-get install -y python3-distutils
+
 sudo apt-get install -y python3.9-dev
 sudo apt-get install -y python3.9-distutils
+
 sudo apt-get install -y python3.10-dev
 sudo apt-get install -y python3.10-distutils
+
+sudo apt-get install -y python3.11-dev
+sudo apt-get install -y python3.11-distutils
 
 # tools
 sudo apt-get install -y mc xvfb
