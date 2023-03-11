a516eedf7cb674e2a0fbeb1c62a4852050e05fd1
ponty <ponty@home>
2023-03-11 16:44:13 +0100

    PilWrapper: pass xdisplay="" for X11

--- pyscreenshot/plugins/pilwrap.py
+++ pyscreenshot/plugins/pilwrap.py
@@ -1,17 +1,22 @@
 from PIL import __version__
 
 from pyscreenshot.plugins.backend import CBackend
+from pyscreenshot.util import use_x_display
 
 
 class PilWrapper(CBackend):
-    """Windows/MacOS only."""
-
     name = "pil"
 
     def grab(self, bbox=None):
-        from PIL import ImageGrab  # windows and osx only
+        from PIL import ImageGrab
 
-        return ImageGrab.grab(bbox)
+        # https://pillow.readthedocs.io/en/stable/reference/ImageGrab.html
+        # On Linux, if xdisplay is None then gnome-screenshot will be used if it is installed.
+        # To capture the default X11 display instead, pass xdisplay=""
+        xdisplay = None
+        if use_x_display():
+            xdisplay = ""
+        return ImageGrab.grab(bbox, xdisplay=xdisplay)
 
     def backend_version(self):
         return __version__
