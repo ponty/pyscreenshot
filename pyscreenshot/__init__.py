748b7e4582c2fa1908542eedddb576b35bc8340f
ponty <ponty@home>
2023-03-11 16:38:33 +0100

    type hint

--- pyscreenshot/__init__.py
+++ pyscreenshot/__init__.py
@@ -1,4 +1,7 @@
 import logging
+from typing import List, Optional, Tuple
+
+from PIL import Image
 
 from pyscreenshot.about import __version__
 from pyscreenshot.childproc import childprocess_backend_version
@@ -10,7 +13,11 @@ log = logging.getLogger(__name__)
 log.debug("version=%s", __version__)
 
 
-def grab(bbox=None, childprocess=True, backend=None):
+def grab(
+    bbox: Optional[Tuple[int, int, int, int]] = None,
+    childprocess: bool = True,
+    backend: Optional[str] = None,
+) -> "Image":
     """Copy the contents of the screen to PIL image memory.
 
     :param bbox: optional bounding box (x1,y1,x2,y2)
@@ -30,7 +37,7 @@ def grab(bbox=None, childprocess=True, backend=None):
     return backend_grab(backend, bbox, childprocess)
 
 
-def backends():
+def backends() -> List[str]:
     """Back-end names as a list.
 
     :return: back-ends as string list
@@ -38,7 +45,7 @@ def backends():
     return list(backend_dict.keys())
 
 
-def backend_version(backend):
+def backend_version(backend: str) -> str:
     """Back-end version.
 
     :param backend: back-end (examples:scrot, wx,..)
